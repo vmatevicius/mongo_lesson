4929f6182f601303c0d6cd80f3a0e1ca5582e54c
vmatevicius <vytis765@gmail.com>
2023-05-15 20:09:12 +0300

    added type anotations

--- database_class.py
+++ database_class.py
@@ -1,12 +1,15 @@
 from pymongo import MongoClient
-from typing import Dict, Any
+from pymongo.collection import Collection
+from typing import Dict, Any, List
+from bson.objectid import ObjectId
 
 class TaskManager():
-    def __init__(self, host:str, port:int, db_name:str, collection_name: str):
+    def __init__(self, host:str, port:int, db_name:str, collection_name: str) -> None:
         self.client = MongoClient(host, port)
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
     
     def create_task(self, task: Dict[str, Any]) -> str:
         result = self.collection.insert_one(task)
-        return str(result.inserted_id)        
+        return str(result.inserted_id)
+    
