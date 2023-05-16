bd74ae44617b8c030be1f2b6febf1920c9c6f83d
vmatevicius <vytis765@gmail.com>
2023-05-16 18:15:30 +0300

    Created task manager class with basic operations

--- database_class.py
+++ database_class.py
@@ -1,7 +1,5 @@
 from pymongo import MongoClient
-from pymongo.collection import Collection
 from typing import Dict, Any, List
-from bson.objectid import ObjectId
 
 class TaskManager():
     def __init__(self, host:str, port:int, db_name:str, collection_name: str) -> None:
@@ -13,3 +11,17 @@ class TaskManager():
         result = self.collection.insert_one(task)
         return str(result.inserted_id)
     
+    def get_all_tasks(self) -> List[Dict[str, Any]]:
+        return list(self.collection.find())
+    
+    def get_task(self, task_name: str) -> Dict[str, Any]:
+        return self.collection.find({"task_name": task_name})
+    
+    def update_task(self, task_name: str, task_updates: Dict[str, Any]) -> bool:
+        result = self.collection.update_one({"task_name": task_name}, {"$set": task_updates})
+        return result.modified_count > 0
+        
+    def delete_task(self, task_name: str) -> bool:
+        result = self.collection.delete_one({"task_name": task_name})
+        return result.deleted_count > 0
+        
